f09ffc5017e143f75e76782ce4a66e9ab4f18508
Andy Armstrong <andy@hexten.net>
2016-06-14 14:04:27 +0100

    Remove unused.

--- bin/sprites.sh
+++ bin/sprites.sh
@@ -3,13 +3,8 @@
 ts="/data/d3/mpegts"
 asset="/data/d3/asset"
 
-# find "$ts" -name '*.mpegts' -mmin +10 | while read mpeg; do
-#   perl bin/make-sprites.pl --output "$asset" "$mpeg"
-# done
-
 find "$ts" -name '*.mpegts' -mmin +10 -print0 \
   | xargs -0 -n 1 -P 10                       \
       perl bin/make-sprites.pl --output "$asset"
-
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
